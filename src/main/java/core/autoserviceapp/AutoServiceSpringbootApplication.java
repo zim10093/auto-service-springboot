48f62734eefdcdc46ec320d7c054fc06d32af921
Ihor Bondar <zim20093@gmail.com>
2022-09-05 14:25:11 +0300

    first commit

---
+++ src/main/java/core/autoserviceapp/AutoServiceSpringbootApplication.java
@@ -0,0 +1,12 @@
+package core.autoserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoServiceSpringbootApplication.class, args);
+    }
+}
---
+++ src/test/java/core/autoserviceapp/AutoServiceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package core.autoserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
